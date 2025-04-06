332018e4bd8dfe97db8a59031ae554d17b202540
Sophia Alexander <32229134+Yxxsung@users.noreply.github.com>
2025-04-06 13:52:43 -0400

    code update

--- Arrays.cs
+++ Arrays.cs
@@ -1,2 +1,18 @@
 //Sophia Alexander
 //Last edited 04/06/2025
+
+
+using System;
+
+namespace Exams
+{
+    class ScoreArray
+    {
+        static void Main()
+        {
+            int[] scores = {40, 80, 50, 75, 60, 55}
+
+            Console.WriteLine(scores)
+        }
+    }
+}
