54c159e75fcf5687bef83cafbf2ad9e62907be04
Sophia Alexander <32229134+Yxxsung@users.noreply.github.com>
2025-04-07 12:27:49 -0400

    assignment finished

--- Lists.cs
+++ Lists.cs
@@ -1,2 +1,23 @@
 //Sophia Alexander
-//Last edited 04/06/2025
+//Last edited 04/07/2025
+
+using System;
+using System.console;
+
+namespace Exams
+{
+    class ScoreArray
+    {
+        static void Main()
+        {
+            int[] scores = {40, 80, 50, 75, 60, 55}
+
+            Array.Sort(scores);
+
+            foreach(int i in scores)
+            {
+                Console.WriteLine(i)
+            }
+        }
+    }
+}
